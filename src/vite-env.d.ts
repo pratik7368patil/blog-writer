0963a6309808151a8771059f4f62cb1d6f6e8ea1
Pratik Patil <pratik7368patil@gmail.com>
2023-11-19 14:54:16 +0530

    Added module decleration for non typescript plugins

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,1 +1,35 @@
 /// <reference types="vite/client" />
+declare module "@editorjs/header" {
+  import Header from "@editorjs/header";
+  export = Header;
+}
+
+declare module "@editorjs/list" {
+  import List from "@editorjs/list";
+  export = List;
+}
+
+declare module "@editorjs/marker" {
+  import Marker from "@editorjs/marker";
+  export = Marker;
+}
+
+declare module "@editorjs/raw" {
+  import RawTool from "@editorjs/raw";
+  export = RawTool;
+}
+
+declare module "@editorjs/checklist" {
+  import Checklist from "@editorjs/checklist";
+  export = Checklist;
+}
+
+declare module "@editorjs/embed" {
+  import Embed from "@editorjs/embed";
+  export = Embed;
+}
+
+declare module "@editorjs/quote" {
+  import Quote from "@editorjs/quote";
+  export = Quote;
+}
